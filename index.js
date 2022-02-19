b04948bf8e816dbaff12d6517c96d493e1fd1475
hjohnsick <hjohnsick@yahoo.com>
2022-02-19 12:11:22 -0500

    added skeleton inquirer

--- index.js
+++ index.js
@@ -11,12 +11,39 @@ const ascii = `%c
 `;
 
 console.log(ascii);
-const prompts = () => {
+const startApplication = () => {
     return inquirer.prompt([
         {
             type: "list",
             name: "choice",
-            message: "What would you like to do?"
+            message: "What would you like to do?",
+            choices: [
+                "Add Employee",
+                "Update Employee Role",
+                "View All Roles",
+                "Add Role",
+                "View All Departments",
+                "Add Department",
+                "Exit"
+            ]
         }
     ])
-}
+    .then(({ choice} ) => {
+        if (choice === "Add Employee") {
+
+        } else if (choice === "Update Employee Role") {
+
+        } else if (choice === "View All Roles") {
+
+        } else if (choice === "Add Role") {
+
+        } else if (choice === "View All Departments") {
+
+        } else {
+            console.log("Have a nice day!");
+            return;
+        }
+    });
+};
+
+startApplication();
