677d2b5951a0339083d49e8752fb3694889d5e87
hjohnsick <hjohnsick@yahoo.com>
2022-02-20 14:02:33 -0500

    created prompt to add employee

--- index.js
+++ index.js
@@ -1,66 +1,82 @@
 const inquirer = require('inquirer');
 const Query = require('./lib/Query');
-const db = require('./db/connection');
-// const ascii = `%c
-// .-------------------------------------------------------------------------------.
-// |																				|
-// |																				|
-// |		________					    __										|
-// |	    |		|__  __  ___   _ ___   |  | ____    __     __   ____  ____		|
-// |		| ______| \'__ \` __ \| '__ \  |  |/    \  |  |   |  | /    \/    \     |
-// |		|	 |  |  |  |  |  | | |  ) | |  |                                     |
-// `;
 
-// console.log(ascii);
-// const startApplication = () => {
-//     return inquirer.prompt([
-//         {
-//             type: "list",
-//             name: "choice",
-//             message: "What would you like to do?",
-//             choices: [
-//                 "Add Employee",
-//                 "Update Employee Role",
-//                 "View All Roles",
-//                 "Add Role",
-//                 "View All Departments",
-//                 "Add Department",
-//                 "View All Employees",
-//                 "Quit"
-//             ]
-//         }
-//     ])
-//     .then(({ choice} ) => {
-//         if (choice === "Add Employee") {
-//             console.log("You added an employee");
-//             return startApplication();
-//         } else if (choice === "Update Employee Role") {
-//             console.log("You updated an employee");
-//             return startApplication();
-//         } else if (choice === "View All Roles") {
-//             const role = new Query();
-//             role.viewAllRoles();
-//             console.log("You viewed all roles");
-//             return startApplication();
-//         } else if (choice === "Add Role") {
-//             console.log("you added a role");
-//             return startApplication();
-//         } else if (choice === "View All Departments") {
-//             console.log("you viewed all departments")
-//             return startApplication();
-//         } else if (choice === "View All Employees") {
-//             console.log("You viewed all employees");
-//             return startApplication();
-//         } 
-//         else {
-//             console.log("Have a nice day!");
-//             return;
-//         }
-//     });
-// };
+const employeePrompts = (query, startApplication) => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "firstName",
+            message: "Enter employee's first name:"
+        },
+        {
+            type: "input",
+            name: "lastName",
+            message: "Enter employee's last name:"
+        },
+        {
+            type: "input",
+            name: "roleId",
+            message: "Enter employee's role id:"
+        },
+        {
+            type: "list",
+            name: "managerId",
+            choices:[
+                {
+                    name:"Randy T",
+                    value:1
+                },
+                {
+                    name:"None",
+                    value:null
+                }
+            ],
+            message: "Enter employee's manager id:"
+        }
+    ]).then(({firstName, lastName, roleId, managerId}) => {
+        query.addEmployee(firstName, lastName, roleId, managerId, startApplication);
+    })
+};
 
-// startApplication();
+const startApplication = () => {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: [
+                "Add Employee",
+                "Update Employee Role",
+                "View All Roles",
+                "Add Role",
+                "View All Departments",
+                "Add Department",
+                "View All Employees",
+                "Quit"
+            ]
+        }
+    ])
+    .then(({ choice} ) => {
+        const query = new Query();
+        if (choice === "Add Employee") {
+            employeePrompts(query, startApplication)
+        } else if (choice === "Update Employee Role") {
+            query.updateEmployeeRole(startApplication);
+        } else if (choice === "View All Roles") {
+           query.viewAllRoles(startApplication);
+        } else if (choice === "Add Role") {
+           query.addRoles(startApplication);
+        } else if (choice === "View All Departments") {
+             query.viewDepartments(startApplication)
+        } else if (choice === "View All Employees") {
+            query.viewAllEmployees(startApplication);
+        } 
+        else {
+            console.log("Have a nice day!");
+            return;
+        }
+    });
+};
 
-// query.viewAllRoles();
-const query = new Query();
-query.viewAllEmployees();
+startApplication();
+
