69e9bba55bf4705138e9ae6315a23c48c271704a
hjohnsick <hjohnsick@yahoo.com>
2022-02-20 07:31:40 -0500

    fixed connection to db

--- db/connection.js
+++ db/connection.js
@@ -6,9 +6,11 @@ const db = mysql.createConnection(
         host: 'localhost',
         user: process.env.DB_USER,
         password: process.env.DB_PW,
-        database: process.env.DDB_NAME
+        database: process.env.DB_NAME
     },
-    console.log('Connected to the employee tracker database.')
 );
-
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to the employee tracker database.');
+})
 module.exports = db;
--- index.js
+++ index.js
@@ -1,59 +1,66 @@
 const inquirer = require('inquirer');
+const Query = require('./lib/Query');
+const db = require('./db/connection');
+// const ascii = `%c
+// .-------------------------------------------------------------------------------.
+// |																				|
+// |																				|
+// |		________					    __										|
+// |	    |		|__  __  ___   _ ___   |  | ____    __     __   ____  ____		|
+// |		| ______| \'__ \` __ \| '__ \  |  |/    \  |  |   |  | /    \/    \     |
+// |		|	 |  |  |  |  |  | | |  ) | |  |                                     |
+// `;
 
-const ascii = `%c
-.-------------------------------------------------------------------------------.
-|																				|
-|																				|
-|		________					    __										|
-|	    |		|__  __  ___   _ ___   |  | ____    __     __   ____  ____		|
-|		| ______| \'__ \` __ \| '__ \  |  |/    \  |  |   |  | /    \/    \     |
-|		|	 |  |  |  |  |  | | |  ) | |  |                                     |
-`;
+// console.log(ascii);
+// const startApplication = () => {
+//     return inquirer.prompt([
+//         {
+//             type: "list",
+//             name: "choice",
+//             message: "What would you like to do?",
+//             choices: [
+//                 "Add Employee",
+//                 "Update Employee Role",
+//                 "View All Roles",
+//                 "Add Role",
+//                 "View All Departments",
+//                 "Add Department",
+//                 "View All Employees",
+//                 "Quit"
+//             ]
+//         }
+//     ])
+//     .then(({ choice} ) => {
+//         if (choice === "Add Employee") {
+//             console.log("You added an employee");
+//             return startApplication();
+//         } else if (choice === "Update Employee Role") {
+//             console.log("You updated an employee");
+//             return startApplication();
+//         } else if (choice === "View All Roles") {
+//             const role = new Query();
+//             role.viewAllRoles();
+//             console.log("You viewed all roles");
+//             return startApplication();
+//         } else if (choice === "Add Role") {
+//             console.log("you added a role");
+//             return startApplication();
+//         } else if (choice === "View All Departments") {
+//             console.log("you viewed all departments")
+//             return startApplication();
+//         } else if (choice === "View All Employees") {
+//             console.log("You viewed all employees");
+//             return startApplication();
+//         } 
+//         else {
+//             console.log("Have a nice day!");
+//             return;
+//         }
+//     });
+// };
 
-console.log(ascii);
-const startApplication = () => {
-    return inquirer.prompt([
-        {
-            type: "list",
-            name: "choice",
-            message: "What would you like to do?",
-            choices: [
-                "Add Employee",
-                "Update Employee Role",
-                "View All Roles",
-                "Add Role",
-                "View All Departments",
-                "Add Department",
-                "View All Employees",
-                "Quit"
-            ]
-        }
-    ])
-    .then(({ choice} ) => {
-        if (choice === "Add Employee") {
-            console.log("You added an employee");
-            return startApplication();
-        } else if (choice === "Update Employee Role") {
-            console.log("You updated an employee");
-            return startApplication();
-        } else if (choice === "View All Roles") {
-            console.log("You viewd all roles");
-            return startApplication();
-        } else if (choice === "Add Role") {
-            console.log("you added a role");
-            return startApplication();
-        } else if (choice === "View All Departments") {
-            console.log("you viewed all departments")
-            return startApplication();
-        } else if (choice === "View All Employees") {
-            console.log("You viewed all employees");
-            return startApplication();
-        } 
-        else {
-            console.log("Have a nice day!");
-            return;
-        }
-    });
-};
+// startApplication();
 
-startApplication();
+// query.viewAllRoles();
+const query = new Query();
+query.viewAllEmployees();
